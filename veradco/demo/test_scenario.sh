0229dd303fa4d03fc0bfeee5001c6a684fdcac72
smart duck <smart.duck.dev@gmail.com>
2022-09-20 22:28:35 +0200

    test_scenario.sh can be launched from every where

--- veradco/demo/test_scenario.sh
+++ veradco/demo/test_scenario.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+script_abs_path="$(dirname $(readlink -f $0))"
+
 kubectl create ns ds-ns
-kubectl apply -f deployments/01_namespaces.yaml
+kubectl apply -f $script_abs_path/deployments/01_namespaces.yaml
 
 while true; do \
 
-  kubectl delete -f pods/01_fail_pod_creation_test.yaml
-  kubectl delete -f pods/02_fail_pod_creation_test.yaml
-  kubectl delete -f pods/02_success_pod_creation_test.yaml
-  kubectl delete -f pods/03_success_pod_creation_test_special.yaml
-  kubectl delete -f deployments/02_fail_deployment_creation.yaml
-  kubectl delete -f deployments/03_sucess_deployment_creation.yaml
-  kubectl delete -f daemonsets/daemonset.yaml
+  kubectl delete -f $script_abs_path/pods/01_fail_pod_creation_test.yaml
+  kubectl delete -f $script_abs_path/pods/02_fail_pod_creation_test.yaml
+  kubectl delete -f $script_abs_path/pods/02_success_pod_creation_test.yaml
+  kubectl delete -f $script_abs_path/pods/03_success_pod_creation_test_special.yaml
+  kubectl delete -f $script_abs_path/deployments/02_fail_deployment_creation.yaml
+  kubectl delete -f $script_abs_path/deployments/03_sucess_deployment_creation.yaml
+  kubectl delete -f $script_abs_path/daemonsets/daemonset.yaml
 
-  kubectl apply -f pods/01_fail_pod_creation_test.yaml
-  kubectl apply -f pods/02_fail_pod_creation_test.yaml
-  kubectl apply -f pods/02_success_pod_creation_test.yaml
-  kubectl apply -f pods/03_success_pod_creation_test_special.yaml
-  kubectl apply -f deployments/02_fail_deployment_creation.yaml
-  kubectl apply -f deployments/03_sucess_deployment_creation.yaml
-  kubectl apply -f daemonsets/daemonset.yaml
+  kubectl apply -f $script_abs_path/pods/01_fail_pod_creation_test.yaml
+  kubectl apply -f $script_abs_path/pods/02_fail_pod_creation_test.yaml
+  kubectl apply -f $script_abs_path/pods/02_success_pod_creation_test.yaml
+  kubectl apply -f $script_abs_path/pods/03_success_pod_creation_test_special.yaml
+  kubectl apply -f $script_abs_path/deployments/02_fail_deployment_creation.yaml
+  kubectl apply -f $script_abs_path/deployments/03_sucess_deployment_creation.yaml
+  kubectl apply -f $script_abs_path/daemonsets/daemonset.yaml
 done
