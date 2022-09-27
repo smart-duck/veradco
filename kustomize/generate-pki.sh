8c02f3837207bad74893a8738c38ddcd8140c8fe
Laurent Charnot <laurent.charnot@continental-corporation.com>
2022-09-27 13:16:18 +0200

    Generate 10 years certificates

--- kustomize/generate-pki.sh
+++ kustomize/generate-pki.sh
@@ -5,9 +5,9 @@ CURRDIR=$(dirname $(readlink -f $0))
 echo "Creating certificates"
 rm -Rf $CURRDIR/base/certs/
 mkdir -p $CURRDIR/base/certs/
-openssl req -nodes -new -x509 -keyout $CURRDIR/base/certs/ca.key -out $CURRDIR/base/certs/ca.crt -subj "/CN=Veradco Admission Controller CA"
+openssl req -nodes -new -x509 -days 3600 -keyout $CURRDIR/base/certs/ca.key -out $CURRDIR/base/certs/ca.crt -subj "/CN=Veradco Admission Controller CA"
 openssl genrsa -out $CURRDIR/base/certs/tls.key 2048
-openssl req -new -key $CURRDIR/base/certs/tls.key -subj "/CN=veradco.veradco.svc" -config $CURRDIR/veradco-cert.conf | openssl x509 -req -CA $CURRDIR/base/certs/ca.crt -CAkey $CURRDIR/base/certs/ca.key -CAcreateserial -out $CURRDIR/base/certs/tls.crt -extensions v3_req -extfile $CURRDIR/veradco-cert.conf
+openssl req -new -key $CURRDIR/base/certs/tls.key -subj "/CN=veradco.veradco.svc" -config $CURRDIR/veradco-cert.conf | openssl x509 -req -CA $CURRDIR/base/certs/ca.crt -CAkey $CURRDIR/base/certs/ca.key -CAcreateserial -days 3600 -out $CURRDIR/base/certs/tls.crt -extensions v3_req -extfile $CURRDIR/veradco-cert.conf
 
 export CA_BUNDLE=$(cat $CURRDIR/base/certs/ca.crt | base64 | tr -d '\n')
 
